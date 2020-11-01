7d3a290231038358200d5b57f44ad46e70fb3ed2
michmora <memora@sfu.ca>
2020-10-31 21:39:54 -0700

    Added Starting Functions/Data Types For Supervisor Information
    Functions and Data Types regarding the getting/setting/declaration
    of Supervisor info meant to be stored in the SQL Database added.
    Also coded Supervisor to be a Singleton (could change depending on
    team opinion) since only one user (Supervisor) should be actively
    using the app at a time.

--- app/src/main/java/com/example/supervisor_seerem/model/Supervisor.java
+++ app/src/main/java/com/example/supervisor_seerem/model/Supervisor.java
@@ -2,7 +2,91 @@ package com.example.supervisor_seerem.model;
 
 /**
  *  Supervisor (only for the user of the application)
+ *  Class for defining data realted to the Supervisor object,
+ *  which is meant to relate to a Supervisor's data
+ * @Author Michael Mora
  */
 public class Supervisor {
+    private String savedUsername;
+    private String savedPassword;
+    private String id;
+    private String medicalConsiderations;
+    private String emergencyContactName;
+    private int emergencyContantNumber;
 
+
+    // When creating a new user, their username and password will be the first things saved; everything else will be added manually later on
+    private Supervisor(String username, String password){
+        savedUsername = username;
+        savedPassword = password;
+    }
+
+    private Supervisor(){}
+
+    private static Supervisor instance;
+
+    // Theoretically since there should only be one instance of a
+    // Supervisor stored in data at a time (
+    public static Supervisor getInstance(String username, String password){
+        if(instance == null){
+            instance = new Supervisor(username, password);
+        }
+        return instance;
+    }
+
+    // Could be called when the user logs out, so make the reference null so a new instance of
+    // Supervisor can be instantiated when someone logs in again.
+    public void removeSupervisor(){
+        if(instance != null){
+            instance = null;
+        }
+    }
+
+    public String getSavedUsername() {
+        return savedUsername;
+    }
+
+    public void setSavedUsername(String savedUsername) {
+        this.savedUsername = savedUsername;
+    }
+
+    public String getSavedPassword() {
+        return savedPassword;
+    }
+
+    public void setSavedPassword(String savedPassword) {
+        this.savedPassword = savedPassword;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMedicalConsiderations() {
+        return medicalConsiderations;
+    }
+
+    public void setMedicalConsiderations(String medicalConsiderations) {
+        this.medicalConsiderations = medicalConsiderations;
+    }
+
+    public String getEmergencyContactName() {
+        return emergencyContactName;
+    }
+
+    public void setEmergencyContactName(String emergencyContactName) {
+        this.emergencyContactName = emergencyContactName;
+    }
+
+    public int getEmergencyContantNumber() {
+        return emergencyContantNumber;
+    }
+
+    public void setEmergencyContantNumber(int emergencyContantNumber) {
+        this.emergencyContantNumber = emergencyContantNumber;
+    }
 }
