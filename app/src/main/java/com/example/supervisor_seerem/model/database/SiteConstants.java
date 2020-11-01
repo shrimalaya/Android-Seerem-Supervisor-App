6d41446d22b4fe5f9d1605c24b5ae5043b60eb91
Shri <shrimalaya@gmail.com>
2020-11-01 22:39:32 +0530

    WorkerManager class
    +
    
    Initial implementation of database

---
+++ app/src/main/java/com/example/supervisor_seerem/model/database/SiteConstants.java
@@ -0,0 +1,11 @@
+package com.example.supervisor_seerem.model.database;
+
+public class SiteConstants {
+    public static final String DATABASE_NAME = "Supervisor_app";
+    public static final String TABLE_NAME = "SITETABLE";
+    public static final String UID = "_id";
+    public static final String LATITUDE = "latitude";
+    public static final String LONGITUDE = "longitude";
+    public static final String ID = "id";
+    public static final int DATABASE_VERSION = 10;
+}
