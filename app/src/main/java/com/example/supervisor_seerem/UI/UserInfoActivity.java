a971168db1c23ed4b2a0af59b552f2aad5579765
michmora <memora@sfu.ca>
2020-11-06 03:20:25 -0800

    Created UserInfo UI + Fixed LoginInfoActivity Crash
    Created starting version of UserInfoUI without any functionality added yet.
    Also fixed a bug in LoginInfoActivity where the user's input could be
    compared to a null string (this would happen if the the null string
    came from there being no Username/Password stored in Shared Preferences.

--- app/src/main/java/com/example/supervisor_seerem/UI/UserInfoActivity.java
+++ app/src/main/java/com/example/supervisor_seerem/UI/UserInfoActivity.java
@@ -3,8 +3,13 @@ package com.example.supervisor_seerem.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 import com.example.supervisor_seerem.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserInfoActivity extends AppCompatActivity {
 
@@ -12,5 +17,7 @@ public class UserInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        List<RadioButton> contactRadioButtons = new ArrayList<>();
     }
 }
