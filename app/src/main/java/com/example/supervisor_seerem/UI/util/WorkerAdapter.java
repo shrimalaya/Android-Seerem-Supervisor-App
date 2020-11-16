5e414801ca4a932fafe91d0844ef50581a5812e9
Shri <shrimalaya@gmail.com>
2020-11-16 23:28:29 +0530

    Started implementation of AdditionalInfoActivity

--- app/src/main/java/com/example/supervisor_seerem/UI/util/WorkerAdapter.java
+++ app/src/main/java/com/example/supervisor_seerem/UI/util/WorkerAdapter.java
@@ -1,5 +1,7 @@
 package com.example.supervisor_seerem.UI.util;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,8 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.supervisor_seerem.R;
+import com.example.supervisor_seerem.UI.AdditionalInfoActivity;
+import com.example.supervisor_seerem.UI.SiteMapActivity;
 import com.example.supervisor_seerem.model.CONSTANTS;
 import com.example.supervisor_seerem.model.ModelLocation;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -23,13 +27,15 @@ import static com.example.supervisor_seerem.R.layout.worker_row;
 public class WorkerAdapter extends RecyclerView.Adapter<WorkerAdapter.WorkerViewHolder> {
 
     public List<DocumentSnapshot> mList;
+    Context context;
 
     public WorkerAdapter(List<DocumentSnapshot> mList) { this.mList = mList; }
 
     @NonNull
     @Override
     public WorkerAdapter.WorkerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(worker_row, parent, false);
+        context = parent.getContext();
+        View v = LayoutInflater.from(context).inflate(worker_row, parent, false);
         WorkerViewHolder mHolder = new WorkerViewHolder(v);
         return mHolder;
     }
@@ -37,7 +43,7 @@ public class WorkerAdapter extends RecyclerView.Adapter<WorkerAdapter.WorkerView
     @Override
     public void onBindViewHolder(@NonNull WorkerAdapter.WorkerViewHolder holder, int position) {
 
-        DocumentSnapshot curr = mList.get(position);
+        final DocumentSnapshot curr = mList.get(position);
 
         ModelLocation location = new ModelLocation(curr.getGeoPoint(CONSTANTS.LOCATION_KEY).getLatitude(),
                 curr.getGeoPoint(CONSTANTS.LOCATION_KEY).getLongitude());
@@ -45,10 +51,29 @@ public class WorkerAdapter extends RecyclerView.Adapter<WorkerAdapter.WorkerView
         holder.workerIDTextView.setText(curr.getString(CONSTANTS.ID_KEY));
         holder.firstNameTextView.setText(curr.getString(CONSTANTS.FIRST_NAME_KEY));
         holder.lastNameTextView.setText(curr.getString(CONSTANTS.LAST_NAME_KEY));
+
         holder.locationTextView.setText(location.toString());
+        holder.locationTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context, SiteMapActivity.class);
+                i.putExtra("ID", curr.getString(CONSTANTS.ID_KEY));
+                context.startActivity(i);
+            }
+        });
+
         holder.skillsTextView.setText(curr.getString(CONSTANTS.SKILLS_KEY));
         holder.siteIdTextView.setText(curr.getString(CONSTANTS.WORKSITE_ID_KEY));
         holder.supervisorIdTextView.setText(curr.getString(CONSTANTS.SUPERVISOR_ID_KEY));
+
+        holder.additionalInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context, AdditionalInfoActivity.class);
+                i.putExtra("ID", curr.getString(CONSTANTS.ID_KEY));
+                context.startActivity(i);
+            }
+        });
 
     }
 
@@ -67,6 +92,7 @@ public class WorkerAdapter extends RecyclerView.Adapter<WorkerAdapter.WorkerView
         public TextView skillsTextView;
         public TextView siteIdTextView;
         public TextView supervisorIdTextView;
+        public TextView additionalInfo;
 
         public WorkerViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -79,22 +105,8 @@ public class WorkerAdapter extends RecyclerView.Adapter<WorkerAdapter.WorkerView
             skillsTextView = itemView.findViewById(R.id.workerRow_Skills);
             siteIdTextView = itemView.findViewById(R.id.workerRow_WorksiteId);
             supervisorIdTextView = itemView.findViewById(R.id.workerRow_supervisor);
+
+            additionalInfo = itemView.findViewById(R.id.WorkerRow_fix_additionalInfo);
         }
     }
 }
-
-
-//    List<String> skills = curr.getSkills();
-//    String sk = "";
-//        for(String elt: skills) {
-//                sk = sk + elt + ", ";
-//                }
-//
-//                holder.workerIDTextView.setText(curr.getEmployeeID());
-//                holder.firstNameTextView.setText(curr.getFirstName());
-//                holder.lastNameTextView.setText(curr.getLastName());
-//                holder.locationTextView.setText(curr.getLocation().toString());
-//                holder.skillsTextView.setText(sk);
-//                holder.siteIdTextView.setText(curr.getWorksiteID());
-//                holder.supervisorIdTextView.setText(curr.getSupervisorID());
-
