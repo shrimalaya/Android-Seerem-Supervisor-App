ed44af48f5807e11b3bfd709e238b6ab405779ab
michmora <memora@sfu.ca>
2020-11-16 22:05:20 -0800

    Made CommunicationActivity To Display Text According To Intents
    Made it so that CommunicationActivity would be passed an Intent which
    contains data about a worker's contact means.

--- app/src/main/java/com/example/supervisor_seerem/UI/CommunicationActivity.java
+++ app/src/main/java/com/example/supervisor_seerem/UI/CommunicationActivity.java
@@ -8,45 +8,155 @@ import android.os.Bundle;
 import android.text.util.Linkify;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.supervisor_seerem.R;
+import com.example.supervisor_seerem.model.CONSTANTS;
+import com.example.supervisor_seerem.model.DocumentManager;
+import com.example.supervisor_seerem.model.Worker;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 /**
  * Displays different way through which the user can talk to others.
  */
 public class CommunicationActivity extends AppCompatActivity implements View.OnClickListener{
 
+    TextView communicationRecipient;
     TextView displayedPhoneNumber;
     TextView displayedEmail;
     TextView displayedMeets;
     TextView displayedTeams;
     TextView displayedSkype;
+    TextView displayedZoom;
+    Button goToMeets;
+    Button goToTeams;
+    Button goToSkype;
+    Button goToZoom;
+
+    String employeeFirstName;
+    String employeeLastName;
+    String employeeFullName;
+    String employeePhoneNumber;
+    String employeeEmail;
+    String employeeMEETS;
+    String employeeTEAMS;
+    String employeeSkype;
+    String employeeZoom;
+
+    int employeeClickablePhoneNumber;
+
+    FirebaseFirestore firestoreReference = FirebaseFirestore.getInstance();
+    private DocumentManager manager = DocumentManager.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communication);
+        //List<DocumentSnapshot> contacts = manager.getContacts();
+
         setupNavigationBar();
+        retrieveIntent();
         setUpTextViews();
+    }
+
+//    private interface DocListCallback{
+//        void onCallback(List<DocumentSnapshot> docs);
+//    }
+
+    // Intent should include fields in Contact which the user was able to pass.
+    private void retrieveIntent(){
+        employeeFirstName = getIntent().getStringExtra("EMPLOYEE_FIRST_NAME");
+        employeeLastName = getIntent().getStringExtra("EMPLOYEE_LAST_NAME");
+        employeePhoneNumber = getIntent().getStringExtra("EMPLOYEE_PHONE_NUMBER");
+        employeeEmail = getIntent().getStringExtra("EMPLOYEE_EMAIL");
+
+        //These should be passed as with implicit intents somehow to use with the appropriate app.
+        employeeMEETS = getIntent().getStringExtra("EMPLOYEE_MEETS");
+        employeeTEAMS = getIntent().getStringExtra("EMPLOYEE_TEAMS");
+        employeeZoom = getIntent().getStringExtra("EMPLOYEE_ZOOM");
+
     }
 
     // Some sort of identification to tell which employee the user wants to contact
     private void setUpTextViews(){
+        communicationRecipient = findViewById(R.id.displayCommunicationRecipient);
         displayedPhoneNumber = findViewById(R.id.linkedPhoneNumber);
         displayedEmail = findViewById(R.id.linkedEmail);
         displayedMeets = findViewById(R.id.linkedGoogleMeet);
         displayedTeams = findViewById(R.id.linkedTeams);
         displayedSkype = findViewById(R.id.linkedSkype);
+        displayedZoom = findViewById(R.id.linkedZoom);
 
+        goToMeets = findViewById(R.id.buttonGoogleMeet);
+        goToTeams = findViewById(R.id.buttonMicrosoftTeams);
+        goToSkype = findViewById(R.id.buttonSkype);
+        goToZoom = findViewById(R.id.buttonZoom);
+
+
+        if(employeeLastName == null || employeeFirstName == null){
+            employeeFullName = getString(R.string.communication_no_employee);
+        }else{
+            employeeFullName = getString(R.string.employee_last_name_first_name, employeeLastName, employeeFirstName);
+        }
         // Testing autolink
         // Phone number doesn't work yet...
-        displayedPhoneNumber.setText(""+ 1234567890);
-        displayedEmail.setText("test@gmail.com");
-        displayedPhoneNumber.setAutoLinkMask(Linkify.PHONE_NUMBERS);
+        if(employeePhoneNumber == null){
+            employeePhoneNumber = getString(R.string.communication_no_phone);
+        }
 
+        if(employeeEmail == null){
+            employeeEmail = getString(R.string.communication_no_email);
+        }
+
+        if(employeeMEETS == null){
+            employeeMEETS = getString(R.string.communication_no_google_meet);
+            goToMeets.setEnabled(false);
+        }else{
+            employeeMEETS = getString(R.string.communication_google_meet_exists);
+            goToMeets.setEnabled(true);
+        }
+
+        if(employeeTEAMS == null){
+            employeeTEAMS = getString(R.string.communication_no_microsoft_teams);
+            goToTeams.setEnabled(false);
+        }else{
+            employeeTEAMS = getString(R.string.communication_microsoft_teams_exists);
+            goToMeets.setEnabled(true);
+        }
+
+        if(employeeSkype == null){
+            employeeSkype = getString(R.string.communication_no_microsoft_teams);
+            goToTeams.setEnabled(false);
+        }else{
+            employeeSkype = getString(R.string.communication_microsoft_teams_exists);
+            goToMeets.setEnabled(true);
+        }
+
+        if(employeeZoom == null){
+            employeeZoom = getString(R.string.communication_no_zoom);
+            goToZoom.setEnabled(false);
+        }else{
+            employeeZoom = getString(R.string.communication_zoom_exists);
+            goToZoom.setEnabled(true);
+        }
+
+        communicationRecipient.setText(employeeFullName);
+        displayedPhoneNumber.setText(employeePhoneNumber);
+        displayedEmail.setText(employeeEmail);
+        displayedPhoneNumber.setAutoLinkMask(Linkify.PHONE_NUMBERS);
+        displayedMeets.setText(employeeMEETS);
+        displayedTeams.setText(employeeTEAMS);
+        displayedSkype.setText(employeeSkype);
+        displayedZoom.setText(employeeZoom);
     }
 
     private void setupNavigationBar() {
@@ -90,6 +200,20 @@ public class CommunicationActivity extends AppCompatActivity implements View.OnC
         });
     }
 
+      // Probably unnecessary since this activity should have the data passed to it from another
+      // activity (which knows what specific user is being checked for example.
+//    private void getContactData(final DocListCallback callback){
+//        firestoreReference.collection(CONSTANTS.CONTACT_INFO_COLLECTION)
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if(task.isComplete()){
+//                            callback.onCallback(task.getResult().getDocuments());
+//                        }
+//                    }
+//                });
+//    }
     // For apps which autolink do not cover, we will have to creat Implicit Intents ourselves.
     @Override
     public void onClick(View view) {
